623f361a58e4da0e636a749e1f58b3d747a0c11a
Mr D <github.djb@gmail.com>
2025-10-09 13:25:43 +0700

    initialization frontend

---
+++ frontend_app/.vite/deps/@tensorflow_tfjs-backend-webgl.js
@@ -0,0 +1,22 @@
+import {
+  GPGPUContext,
+  MathBackendWebGL,
+  forceHalfFloat,
+  gpgpu_util_exports,
+  setWebGLContext,
+  version,
+  webgl,
+  webgl_util_exports
+} from "./chunk-74KPZJV4.js";
+import "./chunk-5PKCZ6IZ.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  GPGPUContext,
+  MathBackendWebGL,
+  forceHalfFloat,
+  gpgpu_util_exports as gpgpu_util,
+  setWebGLContext,
+  version as version_webgl,
+  webgl,
+  webgl_util_exports as webgl_util
+};
--- frontend_app/src/main.jsx
+++ frontend_app/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react' // <--- UBAH BARIS INI
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // Sekarang JSX ini kenal 'React'
+  <StrictMode> 
     <App />
   </StrictMode>,
 )
