0f83b47cbc69ac639ae4b9137bc347c3c1b8013f
AyDaKryZii <ayyubidanish2020@gmail.com>
2025-10-17 14:32:04 +0700

    fixed

--- backend_api/database/migrations/2025_10_10_031940_create_users_table.php
+++ backend_api/database/migrations/2025_10_10_031940_create_users_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
     public function up(): void
@@ -48,6 +46,7 @@ return new class extends Migration
 
             // Status umum
             $table->boolean('is_active')->default(true);
+            $table->softDeletes();
 
             $table->timestamps();
         });
